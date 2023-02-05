3c7059c2b57cbcc2ec471c6714828d56654c6e4e
Ian <60273972+Iantwinboy@users.noreply.github.com>
2023-02-05 12:56:57 -0700

    Delete components directory

--- components/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = (props: any) => {
-  return (
-    <div className="h-screen flex">
-      <div className="max-w-sm m-auto">{props.children}</div>
-    </div>
-  );
-};
-
-export default Layout;
